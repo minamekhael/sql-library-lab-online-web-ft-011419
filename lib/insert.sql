6fae994fad771e4a84bde0e1e26103bc95f311f6
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-14 19:28:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("lord of the Rings", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+
+INSERT INTO books 
