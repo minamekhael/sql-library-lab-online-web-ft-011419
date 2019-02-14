2d3b9d04bf4aa548bff0e3425020469adef82b58
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-14 19:32:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,6 +4,6 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2)
 INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
 INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1954, 1);
 INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
 INSERT INTO books (title, year, series_id) VALUES 
-INSERT INTO books (title, year, series_id) VALUES
 INSERT INTO books (title, year, series_id) VALUES
