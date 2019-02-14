b4127e170733bf96419a92607aa1369c398ee2fb
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-14 19:32:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("lord of the Rings", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
 
+INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
 INSERT INTO books (title, year, series_id) VALUES 
-INSERT INTO books (title, year, series_id) VALUES
 INSERT INTO books (title, year, series_id) VALUES
 INSERT INTO books (title, year, series_id) VALUES
 INSERT INTO books (title, year, series_id) VALUES
